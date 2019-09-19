c87dbf7dd54c4645b8d427616aae63c5bf078c2a
Melissa Seren Silva <mee_seren@outlook.com>
2019-09-18 22:47:19 -0300

    Em andamento

--- seguinte.php
+++ seguinte.php
@@ -1,15 +1,14 @@
 <?php 
-	error_reporting(0);
 
 	$list = array();
 
-	$root = [1, 5, 2, 7, 4, 3, 8, 6, ''];
+	$root = [1, 2, 3, '', 4, 5, 6, 7, 8];
 
 	$objective = [1, 2, 3, 4, 5, 6, 7, 8, ''];
 
 	function up($root, $element, $coordinates)
 	{
-		
+
 		if(substr($coordinates,1,1) != '1')
 		{
 			$root[$element] = $root[$element + 3];
@@ -36,7 +35,7 @@
 
 	function left($root, $element, $coordinates)
 	{
-		if(substr($coordinates,3,-1) != '1')
+		if(substr($coordinates,3,-1) != '3')
 		{
 			$root[$element] = $root[$element - 1];
 			$root[$element - 1] = '';
@@ -49,7 +48,7 @@
 
 	function right($root, $element, $coordinates)
 	{
-		if(substr($coordinates,3,-1) != '3')
+		if(substr($coordinates,3,-1) != '1')
 		{
 			$root[$element] = $root[$element + 1];
 			$root[$element + 1] = '';
@@ -108,17 +107,29 @@
 		$left = left($root, $element, $position);
 		$right = right($root, $element, $position);
 
-		if($down !== false)
+		if($down !== false){
+			$down['pai'] = $root;
+			$down['movimento'] = 'down';
 			array_push($list, $down);
+		}
 
-		if($up !== false)
+		if($up !== false){
+			$up['pai'] = $root;
+			$up['movimento'] = 'up';
 			array_push($list, $up);
+		}
 
-		if($right !== false)
+		if($right !== false){
+			$right['pai'] = $root;
+			$right['movimento'] = 'right';
 			array_push($list, $right);
+		}
 
-		if($left !== false)
+		if($left !== false){
+			$left['pai'] = $root;
+			$left['movimento'] = 'left';
 			array_push($list, $left);
+		}
 
 		return $list;
 	}
@@ -128,19 +139,17 @@
 		foreach ($root as $i => $value)
 			if($root[$i] != $objective[$i])
 				return false;
-			
+
 		return true;
 	}
 
-	function searchWidth($root,$list, $objective)
+	function searchWidth($root, $list, $objective)
 	{
 		array_push($list, $root);
 		while (count($list) > 0) {
 			$node = $list[0];
-			
-			#Gambiarra
-			unset($list[0]);
-			$list = array_values($list); 
+
+			array_shift($list); 
 
 			if(objectiveTest($node, $objective))
 				return $node;
@@ -149,7 +158,38 @@
 		}		
 	}
 
+	function searchDepth($root, $list, $objective)
+	{
+
+		array_push($list, $root);
+
+		while (count($list) > 0) {
+			$node = $list[0];
+
+			array_pop($list); 
+
+			if(objectiveTest($node, $objective))
+				return $node;
+			else	
+				$list = nextCondition($node, $list);
+		}	
+	}
+
+	function searchDepthLimit($root, $list, $objective, $limit)
+	{
+		array_push($list, $root);
+
+		while (count($list) > 0) {
+			$node = $list[0];
+
+			array_pop($list); 
+
+			if(objectiveTest($node, $objective))
+				return $node;
+			else	
+				$list = nextCondition($node, $list);
+		}
+	}
 	print '<pre>';
-	print_r(searchWidth($root, $list, $objective));
+	print_r(nextCondition($root, $list));
 	exit;
-?>
