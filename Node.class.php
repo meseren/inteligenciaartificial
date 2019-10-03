eb17c35998ea49f1ca9b5aabc1ac92e79cfdf890
Melissa Seren Silva <mee_seren@outlook.com>
2019-10-02 22:28:22 -0300

    Classe Node

---
+++ Node.class.php
@@ -0,0 +1,23 @@
+<?php
+
+class Node
+{
+	public $custo;
+	public $pai;
+	public $movimento;
+	public $profundidade;
+	public $matriz;
+
+
+	public function __construct($custo, $pai, $movimento, $profundidade, $matriz)
+	{
+		$this->custo = $custo;
+		$this->pai = $pai;
+		$this->movimento = $movimento;
+		$this->profundidade = $profundidade;
+		$this->matriz = $matriz;
+	}
+
+}
+
+?>
