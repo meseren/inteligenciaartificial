1480a26f2d0f19e4c02020d65272b4fc9fe329b5
Melissa Seren Silva <mee_seren@outlook.com>
2019-10-09 21:51:54 -0300

    Heurística
    Busca gulosa pela melhor escolha

--- Node.class.php
+++ Node.class.php
@@ -7,15 +7,16 @@ class Node
 	public $movimento;
 	public $profundidade;
 	public $matriz;
+	public $h;
 
-
-	public function __construct($custo = '', $pai = '', $movimento = '', $profundidade = '', $matriz = '')
+	public function __construct($custo = '', $pai = '', $movimento = '', $profundidade = '', $matriz = '', $h = '')
 	{
 		$this->custo = $custo;
 		$this->pai = $pai;
 		$this->movimento = $movimento;
 		$this->profundidade = $profundidade;
 		$this->matriz = $matriz;
+		$this->h = $h;
 	}
 
 }
