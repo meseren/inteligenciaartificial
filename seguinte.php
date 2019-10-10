0d0cfd0f30dbbd2118fd6fd528c01e543e8498cd
Allan Morishigue <allan.morishigue@hotmail.com>
2019-10-09 22:43:00 -0300

    Tradução e início da Busca Heurística

--- seguinte.php
+++ seguinte.php
@@ -1,76 +1,100 @@
 <?php 
 
+	error_reporting(0);
+	
 	require_once 'Node.class.php';
 
-	error_reporting(0);
+	$objetivo = [1, 2, 3, 4, 5, 6, 7, 8, ''];
 
-	$list = array();
-
-	$root = [1, 2, 3, 4, 5, 6, 7, '', 8];
-
-	$objective = [1, 2, 3, 4, 5, 6, 7, 8, ''];
-
-	function up($root, $element, $coordinates)
+	function calculaForaLugar($estado, $objetivo)
 	{
-		if(substr($coordinates,1,1) != '3')
-		{
-			print $element;
-			$root[$element] = $root[$element + 3];
-			$root[$element + 3] = '';
+		$cont = 0;
 
-			return $root;
+		foreach ($estado->matriz as $key => $value)
+			if($objetivo[$key] != $value)
+				$cont++;
+		
+		return $cont;
+	}
+
+	function verificaAdjacencia($no, $vazio, $lista)
+	{
+		if(($no-3) == $vazio)
+			return true;
+		else 
+			if(($no+3) == $vazio)
+				return true;
+			else 
+				if(($no-1) == $vazio)
+					return true;
+				else 
+					if(($no+1) == $vazio)
+						return true;
+
+		return false;
+	}
+
+	function cima($no, $vazio, $coordenadas)
+	{
+		if(substr($coordenadas,1,1) != '3')
+		{
+			print $vazio;
+			$no[$vazio] = $no[$vazio + 3];
+			$no[$vazio + 3] = '';
+
+			return $no;
 		}
 
 		return false;
 	}
 
-	function down($root, $element, $coordinates)
+	function baixo($no, $vazio, $coordenadas)
 	{
-		if(substr($coordinates,1,1) != '1')
+		if(substr($coordenadas,1,1) != '1')
 		{
-			$root[$element] = $root[$element - 3];
-			$root[$element - 3] = '';
+			$no[$vazio] = $no[$vazio - 3];
+			$no[$vazio - 3] = '';
 
-			return $root;
+			return $no;
 		}
 
 		return false;
 	}
 
-	function left($root, $element, $coordinates)
+	function esquerda($no, $vazio, $coordenadas)
 	{
-		if(substr($coordinates,3,-1) != '3')
+		if(substr($coordenadas,3,-1) != '3')
 		{
-			$root[$element] = $root[$element + 1];
-			$root[$element + 1] = '';
+			$no[$vazio] = $no[$vazio + 1];
+			$no[$vazio + 1] = '';
 
-			return $root;
+			return $no;
 		}
 
 		return false;
 	}
 
-	function right($root, $element, $coordinates)
+	function direita($no, $vazio, $coordenadas)
 	{
-		if(substr($coordinates,3,-1) != '1')
+		if(substr($coordenadas,3,-1) != '1')
 		{
-			$root[$element] = $root[$element - 1];
-			$root[$element - 1] = '';
+			$no[$vazio] = $no[$vazio - 1];
+			$no[$vazio - 1] = '';
 
-			return $root;
+			return $no;
 		}
 
 		return false;
 	}
 
-	function returnPositionFree($root)
+	function retornaIndexVazio($no)
 	{
-		foreach ($root->matriz as $key => $value)
+		foreach ($no->matriz as $key => $value)
 			if(empty($value))
 				return $key;
 	}
 
-	function returnCoordinates($index)
+	function retornaCoordenadasVazio($index)
 	{
 		if($index < 3)
 			if($index < 2)
@@ -99,144 +123,149 @@
 					return '[3,3]';
 	}
 
-	function nextCondition($root)
+	function retornaSucessor($raiz)
 	{
-		$list = array();
+		$lista = array();
 
 		#Descobrindo qual linha a posição livre está
-		$index = returnPositionFree($root);
+		$index = retornaIndexVazio($raiz);
 
-		$position = returnCoordinates($index);
+		$coordenadaVazia = retornaCoordenadasVazio($index);
 
-		$left = left($root->matriz, $index, $position);
-		$down = down($root->matriz, $index, $position);
-		$up = up($root->matriz, $index, $position);
-		$right = right($root->matriz, $index, $position);
+		#Faz todos os movimentos
+		$baixo = baixo($raiz->matriz, $index, $coordenadaVazia);
+		$esquerda = esquerda($raiz->matriz, $index, $coordenadaVazia);
+		$cima = cima($raiz->matriz, $index, $coordenadaVazia);
+		$direita = direita($raiz->matriz, $index, $coordenadaVazia);
 
-		if($down !== false){
+		if($baixo !== false){
 			$node = new Node();
-			$node->matriz = $down;
-			$node->pai = $root;
-			$node->movimento = 'down';
-			$node->profundidade = $root->profundidade + 1;
-			$node->custo = $root->custo + 1;
+			$node->matriz = $baixo;
+			$node->pai = $raiz;
+			$node->movimento = 'baixo';
+			$node->profundidade = $raiz->profundidade + 1;
+			$node->custo = $raiz->custo + 1;
 			
-			array_push($list, $node);
+			array_push($lista, $node);
 		}
 
-		if($up !== false){
+		if($cima !== false){
 			$node = new Node();
-			$node->matriz = $up;
-			$node->pai = $root;
-			$node->movimento = 'up';
-			$node->profundidade = $root->profundidade + 1;
-			$node->custo = $root->custo + 1;
-			array_push($list, $node);
+			$node->matriz = $cima;
+			$node->pai = $raiz;
+			$node->movimento = 'cima';
+			$node->profundidade = $raiz->profundidade + 1;
+			$node->custo = $raiz->custo + 1;
+			array_push($lista, $node);
 		}
 
-		if($right !== false){
+		if($direita !== false){
 			$node = new Node();
-			$node->matriz = $right;
-			$node->pai = $root;
-			$node->movimento = 'right';
-			$node->profundidade = $root->profundidade + 1;
-			$node->custo = $root->custo + 1;
+			$node->matriz = $direita;
+			$node->pai = $raiz;
+			$node->movimento = 'direita';
+			$node->profundidade = $raiz->profundidade + 1;
+			$node->custo = $raiz->custo + 1;
 			
-			array_push($list, $node);
+			array_push($lista, $node);
 		}
 
-		if($left !== false){
+		if($esquerda !== false){
 			$node = new Node();
-			$node->matriz = $left;
-			$node->pai = $root;
-			$node->movimento = 'left';
-			$node->profundidade = $root->profundidade + 1;
-			$node->custo = $root->custo + 1;
+			$node->matriz = $esquerda;
+			$node->pai = $raiz;
+			$node->movimento = 'esquerda';
+			$node->profundidade = $raiz->profundidade + 1;
+			$node->custo = $raiz->custo + 1;
 			
-			array_push($list, $node);
+			array_push($lista, $node);
 		}
 
-		return $list;
+		return $lista;
 	}
 
-	function objectiveTest($root, $objective)
+	function testeOjetivo($no, $objetivo)
 	{
-		foreach ($root as $i => $value)
-			if($value != $objective[$i])
+		foreach ($no as $i => $value)
+			if($value != $objetivo[$i])
 				return false;
 			
 		return true;
 	}
 
-	function widthSearch($root, $objective)
+	function buscaLargura($raiz, $objetivo)
 	{
-		$list = array();
-		array_push($list, $root);
+		$lista = array();
+		array_push($lista, $raiz);
 
-		while (count($list) > 0) {
+		while (count($lista) > 0) {
 
-			$node = $list[0];
-			array_shift($list); 
+			$node = $lista[0];
 
-			if(objectiveTest($node->matriz, $objective))
+			#Remove o primeiro da lista
+			array_shift($lista); 
+
+			if(testeOjetivo($node->matriz, $objetivo))
 				return $node;
 			else
-				$list =  nextCondition($node);
+				$lista =  retornaSucessor($node);
 
-			//print_r($list);
+			print_r($lista);
 		}		
 	}
 	
-	function depthSearch($root, $objective)
+	function buscaProfundidade($raiz, $objetivo)
 	{
-		$list = array();
-		array_push($list, $root);
+		$lista = array();
+		array_push($lista, $raiz);
 
-		while (count($list) > 0) {
-			$node = $list[count($list)-1];
+		while (count($lista) > 0) {
+			$node = $lista[count($lista)-1];
 			
-			array_pop($list); 
+			#Remove o último da lista
+			array_pop($lista); 
 
-			if(objectiveTest($node->matriz, $objective))
+			if(testeOjetivo($node->matriz, $objetivo))
 				return $node;
 			else	
-				$list = nextCondition($node);
+				$lista = retornaSucessor($node);
 
-			//print_r($list);
+			//print_r($lista);
 		}	
 	}
 	
-	function depthLimitSearch($root, $objective, $limite)
+	function buscaProfundidadeLimite($raiz, $objetivo, $limite)
 	{
-		$list = array();
-		array_push($list, $root);
+		$lista = array();
+		array_push($lista, $raiz);
 
-		while (count($list) > 0) {
-			$node = $list[count($list)-1];
+		while (count($lista) > 0) {
+			$node = $lista[count($lista)-1];
 
-			array_pop($list); 
+			#Remove o último da lista
+			array_pop($lista); 
 
-			if(objectiveTest($node->matriz, $objective)){
+			if(testeOjetivo($node->matriz, $objetivo)){
 				return $node;
 			}
 			else
 				if($node->profundidade < $limite)
-					$list =  nextCondition($node);
+					$lista =  retornaSucessor($node);
 
-			//print_r($list);
+			//print_r($lista);
 		}
 
 		return null;	
 	}
 	
-	function iterativeDeepeningSearch($root, $objective)
+	function buscaAprofundamentoIterativo($raiz, $objetivo)
 	{
 		$limite = 0;
 		
 		do
 		{
 		   ++$limite;
-		   $r = depthLimitSearch($root, $objective, $limite);
+		   $r = buscaProfundidadeLimite($raiz, $objetivo, $limite);
 		}while(is_null($r));
 		
 		print 'Limite >>> '.$limite.'<br><br>';
@@ -244,46 +273,58 @@
 		return $r;
 	}
 
-	function orderCost($list)
+	function ordenarCusto($lista)
 	{
-		for ($i=0; $i < count($list); $i++){
-			for ($j=1; $j < count($list); $j++){
-				if($list[$i]->custo > $list[$j]->custo)
+		for ($i=0; $i < count($lista); $i++){
+			for ($j=1; $j < count($lista); $j++){
+				if($lista[$i]->custo > $lista[$j]->custo)
 				{
-					$aux = $list[$i];
-					$list[$i] = $list[$j];
-					$list[$j] = $aux;
+					$aux = $lista[$i];
+					$lista[$i] = $lista[$j];
+					$lista[$j] = $aux;
 				}
 			}
 		}
 
-		return $list;
+		return $lista;
 	}
 
-	function custoUniforme($root, $objective)
+	function custoUniforme($raiz, $objetivo)
 	{
 		$list = array();
 
-		array_push($list, $root);
+		array_push($list, $raiz);
 
 		while (count($list) > 0) {
-			$list = orderCost($list);
+			$list = ordenarCusto($list);
 			$node = $list[0];
 
+			#Remove o primeiro da lista
 			array_shift($list); 
 
-			if(objectiveTest($node, $objective))
+			if(testeOjetivo($node, $objetivo))
 				return $list;
 			else	
-				$list = nextCondition($node);
+				$list = retornaSucessor($node);
 			
 			print_r($list);
 		}
 	}
 	
 	
-	$root = new Node(0, '', '', 0, [1, 2, 3, 4, 5, 6, 7, '', 8]);
+	$raiz = new Node(0, '', '', 0, [1, 2, 3, 4, 5, 6, 7, 8, '']);
 
+	$vazio = retornaIndexVazio($raiz);
+
+	if(verificaAdjacencia(8, $vazio))
+		print 'eh adjacente';
+	else 
+		print 'nao eh adjacente';
+	
+	exit;
+
+	print $vazio;
+	exit;
 	print '<pre>';
-	print_r(custoUniforme($root, $objective));
+	print(calculaForaLugar($raiz, $objetivo));
 	
